5f1da907f3c76144c173f23d5a5ddaae6f564d1a
Sczeam <zawhtikeaung19@gmail.com>
2023-07-28 16:56:02 +0100

    fix download path

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ def download():
     video_title = result.stdout.strip()
 
     # Get the absolute path of the "downloads" folder on the server
-    downloads_folder = os.path.abspath('downloads')
+    downloads_folder = '/workspace/downloads'
+
 
     # Construct the full file path on the server
     file_path = os.path.join(downloads_folder, f'{video_title}.{extension}')
