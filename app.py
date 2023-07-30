c9e232d272cb40e90dc3ad944bfadaa55af1a006
Sczeam <zawhtikeaung19@gmail.com>
2023-07-30 21:22:52 +0100

    downloading message appear but not disappear

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import shutil
 import yt_dlp
-from flask import Flask, render_template, request, send_file, make_response, jsonify
-from yt_dlp.utils import DownloadError
+from flask import Flask, render_template, request, send_file, make_response
 
 app = Flask(__name__)
 
@@ -15,8 +14,6 @@ def download():
     url = request.form['url']
     format_choice = request.form['format']
 
-    # Validate the user input and handle errors here if necessary
-
     # Download the video/audio using yt-dlp
     temp_dir = 'temp_downloads'  # Create a temporary directory to store the downloaded file
     os.makedirs(temp_dir, exist_ok=True)
@@ -28,25 +25,22 @@ def download():
             'outtmpl': os.path.join(temp_dir, 'audio.mp3'),
             'progress_hooks': [],
             'playlist_items': '1',
-            # 'ext': 'mp3',  # Explicitly specify the extension for MP3 downloads
         }
     else:
         ydl_opts = {
             "quiet": True,
-            'format': 'bestvideo+bestaudio/best',
+            'format': 'bestvideo+bestaudio[ext=m4a]/best',
             "merge_output_format": "mp4",
-            # 'writethumbnail':True,
             'outtmpl': os.path.join(temp_dir, 'video.%(ext)s'),
             'progress_hooks': [],
             'playlist_items': '1',
         }
 
-    
     current_directory = os.getcwd()  # Get the current working directory
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-    except DownloadError as e:
+    except yt_dlp.utils.DownloadError as e:
         # Handle the error gracefully
         error_message = "The video cannot be downloaded from Instagram currently. Please try again later or download from another URL."
         return render_template('error.html', error_message=error_message)
@@ -71,20 +65,9 @@ def download():
         as_attachment=True,
         download_name=filename,
     ))
-    response.set_cookie('downloadCompleted', 'true')
+    response.set_cookie('downloadCompleted', 'true')  # Set the downloadCompleted cookie
 
     return response
-def mark_download_completed(response):
-    response.headers['Content-Disposition'] = 'attachment; filename=' + filename
-    # Set the downloadCompleted flag to true (using a hidden input field)
-    response.set_cookie('downloadCompleted', 'true')
-    return response
-
-@app.route('/check_download_completion')
-def check_download_completion():
-    # Check if the downloadCompleted cookie is set to true
-    download_completed = request.cookies.get('downloadCompleted', False)
-    return jsonify(download_completed)
 
 @app.route('/termandcondition')
 def termandcondition():
